74eadac8333c563c3a8f8aeeab3cf33a1ffebd89
jakob Tveen <jakobtveen@gmail.com>
2024-01-21 12:30:43 +0100

    Initial commit

---
+++ src/main/java/dk/itveen/HelloService.java
@@ -0,0 +1,12 @@
+package dk.itveen;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebService;
+
+@WebService(name = "HelloService", serviceName = "HelloService")
+public interface HelloService {
+
+    @WebMethod
+    String hello(String text);
+
+}
